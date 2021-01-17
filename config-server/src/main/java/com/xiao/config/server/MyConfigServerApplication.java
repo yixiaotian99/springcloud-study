76d09123876215b1e6c10e92e8062b2a50e024f7
sunjinwei4 <sunjinwei@jd.com>
2021-01-17 18:04:38 +0800

    添加eureka-client读取配置中心

--- config-server/src/main/java/com/xiao/config/server/MyConfigServerApplication.java
+++ config-server/src/main/java/com/xiao/config/server/MyConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.xiao.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author sunjinwei
@@ -15,6 +17,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  **/
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class MyConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyConfigServerApplication.class, args);
