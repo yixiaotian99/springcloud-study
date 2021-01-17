efebb46ab4a75459b263c062d74cd025d041a355
sunjinwei4 <sunjinwei@jd.com>
2021-01-17 17:18:09 +0800

    添加eureka-server

---
+++ eureka-server/src/main/java/com/xiao/eureka/server/MyEurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.xiao.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author sunjinwei
+ * @Date 1/17/21 5:11 PM
+ * @Description 注册中心 服务端
+ *
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class MyEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurekaServerApplication.class, args);
+    }
+}
