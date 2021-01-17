ef21dcba44374ddcda423ddc1abccc9d244f5d84
sunjinwei4 <sunjinwei@jd.com>
2021-01-17 17:29:29 +0800

    添加eureka-client客户端

---
+++ eureka-client/src/main/java/com/xiao/eureka/client/MyClientApplication.java
@@ -0,0 +1,20 @@
+package com.xiao.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author sunjinwei
+ * @Date 1/17/21 5:23 PM
+ * @Description 注册成为 Eureke 客户端服务
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class MyClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyClientApplication.class, args);
+    }
+}
